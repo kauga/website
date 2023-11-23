fbaac7b8e4b516239dd3f498f711014df4c1641a
kauga <alexlucas1944@gmail.com>
2023-11-22 22:32:18 -0800

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+const headerEl = document.querySelector('.unorder-menu');
+const mobileEl = document.querySelector('.menuBars');
+const closeEl = document.querySelector('.times');
+
+mobileEl.addEventListener('click', () => {
+  headerEl.classList.add('hidden');
+  closeEl.classList.remove('hidden');
+  console.log(`kauga`);
+})
